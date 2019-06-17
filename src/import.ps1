768442cb8657c62194953b19ba9ac1e08ab51ce3
Abel Huarca <abel.huarca@avantica.net>
2019-06-17 18:51:58 -0500

    add subscription to security public apis

--- src/import.ps1
+++ src/import.ps1
@@ -63,7 +63,7 @@ $sn = $env:SERVICE_NAME
 
 $ApiMgmtContext = New-AzApiManagementContext -ResourceGroupName $rg -ServiceName $sn
 New-AzApiManagementProduct -Context $ApiMgmtContext -ProductId tenpoapi -Title "Tenpo API" -Description "Tenpo API" -LegalTerms "Free for all" -SubscriptionRequired $False -State "Published"
-New-AzApiManagementProduct -Context $ApiMgmtContext -ProductId tenpoapiSubscription -Title "Tenpo API" -Description "Tenpo API" -LegalTerms "Free for all"  -State "Published"
+New-AzApiManagementProduct -Context $ApiMgmtContext -ProductId tenpoapiSubscription -Title "Tenpo API Subscription" -Description "Tenpo API Subscription" -LegalTerms "Free for all"  -State "Published"
 
 #Set-AzApiManagementProduct -Context $ApiMgmtContext -ProductId unlimited -SubscriptionRequired $False
 #Set-AzApiManagementSubscription -Context $ApiMgmtContext -SubscriptionId -0123456789 -PrimaryKey "80450f7d0b6d481382113073f67822c1" -SecondaryKey "97d6112c3a8f48d5bf0266b7a09a761c" -State "Active"
